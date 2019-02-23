d66e934f0dd0df132e3dd2c8b7693c8242cfc69d
sarkar10425 <43996902+sarkar10425@users.noreply.github.com>
2019-02-24 00:36:54 +0530

    Update Face Recognition.py

--- Face Recognition.py
+++ Face Recognition.py
@@ -15,7 +15,7 @@ def detect(gray,frame):
         cv2.rectangle((x,y),(x+w,y+h),(255,0,0),2)
         roi_gray=gray[y:y+h,x:x+w]
         roi_color=frame[y:y+h,x:x+w]
-        eyes=eye_cascade.detectMultiScale(roi_grey,1.1,3)
+        eyes=eye_cascade.detectMultiScale(roi_grey,1.1,22)
         for(ex,ey,ew,eh) in eyes:
             cv2.rectangle((ex,ey),(ex+ew,ey+eh),(0,255,0),2)
     return frame
